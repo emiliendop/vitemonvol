3767d549538a4f2c8da945bda1ba76b986b03545
emiliendop <emiliendop10@gmail.com>
2022-03-30 03:32:53 +0100

    page d'accueil

---
+++ View/home.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HOME</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../Public/css/w3.css">
+<link rel="stylesheet" href="../Public/css">
+<link rel="stylesheet" href="../Public/css/home.css">
+<style>
+	body, html,h1{height: 100%;
+font-style: italic;
+font-family: "Amatic SC", sans-serif;}
+</style>
+</head>
+<body>
+  <nav>
+  
+     
+      <div class="w3-top">
+  <div class="w3-bar  w3-black w3-xlarge">
+  <a href="home.php" class="w3-bar-item w3-button"><i>Accueil</i></a>
+		  <a href="#" class="w3-bar-item w3-button"><i>Circuits</i></a>
+		  <a href="#about" class="w3-bar-item w3-button"><i>A Propos</i></a>
+		  <a href="#contact" class="w3-bar-item w3-button"><i>Contact</i></a>
+		  <div style="float: right!important;">
+			<a class="w3-bar-item "><i> AGENCE VOYAGE</i></a>
+		  </div>
+  </div>
+		
+  </nav>
+
+
+<div class="w3-display-container" style="margin-bottom:50px">
+  <img src="../Public/images/vol.jpeg" style="width:100%" class="dim">
+  <div class="w3-display-bottomleft w3-container w3-amber  w3-hide-small"
+   style="bottom:5%;opacity:0.7;width:70%">
+  <h2><b>Voyagez à travers le monde<br>comme vous l'imaginez </b></h2>
+</div>
+</div>
+
+<div class="w3-row w3-container" style="margin:50px 0">
+<div class="w3-third w3-container">
+  <div class="w3-topbar w3-border-amber">
+    <img src="../Public/images/archipel.jpg" style="width:100%" class="img">
+    <h2>Voyage sur l'achipel nippon</h2>
+    <a href=""><p>PLUS DE DETAILS</p></a>
+  </div>
+</div>
+
+<div class="w3-third w3-container">
+  <div class="w3-topbar w3-border-amber">
+    <img src="../Public/images/grece.jpg" style="width:100%" class="img">
+    <h2>Visite historique en Grèce</h2>
+    <a href=""><p>PLUS DE DETAILS</p></a>
+  </div>
+</div>
+
+<div class="w3-third w3-container">
+  <div class="w3-topbar w3-border-amber">
+    <img src="../Public/images/caribou.jpg" style="width:100%"  class="img">
+    <h2>Voyage au pays des caribous</h2>
+    <a href=""><p>PLUS DE DETAILS</p></a>
+  </div>
+</div>
+</div>
+
+
+
+
+
+</div>
+
+<footer class="w3-container w3-padding-32 w3-grey">  
+    <div class="w3-row-padding">
+      
+    
+      <div class="w3-third">
+        <h3>GALLERIES</h3>
+        <ul class="w3-ul">
+          <li class="w3-padding-16 w3-hover-black">
+            <img src="/w3images/workshop.jpg" class="w3-left w3-margin-right" style="width:50px">
+            <span class="w3-large">Lorem</span><br>
+            <span>Sed mattis nunc</span>
+          </li>
+          <li class="w3-padding-16 w3-hover-black">
+            <img src="/w3images/gondol.jpg" class="w3-left w3-margin-right" style="width:50px">
+            <span class="w3-large">Ipsum</span><br>
+            <span>Praes tinci sed</span>
+          </li> 
+        </ul>
+      </div>
+
+      <div class="w3-third">
+        <h3>PAYS POPULAIRES</h3>
+        <p>
+          <span class="w3-tag w3-black w3-margin-bottom">DESTINATION</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">New York</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">London</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">IKEA</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">NORWAY</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">DIY</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Ideas</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Baby</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Family</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">News</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Clothing</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Shopping</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Sports</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Games</span>
+        </p>
+      </div>
+      <div class="w3-third">
+      <a href="home.php" class="w3-button w3-light-grey w3-xlarge w3-center"><i class="fa fa-arrow-up w3-margin-right"></i>revenir en haut</a>
+  <br>
+  <div class="w3-xlarge w3-padding-32">
+    <i class="fa fa-facebook-official w3-hover-opacity w3-margin-right"></i>
+    <i class="fa fa-instagram w3-hover-opacity w3-margin-right"></i>
+    <i class="fa fa-snapchat w3-hover-opacity w3-margin-right"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity w3-margin-right"></i>
+    <i class="fa fa-twitter w3-hover-opacity w3-margin-right"></i>
+    <i class="fa fa-linkedin w3-hover-opacity w3-margin-right"></i></div>
+    <div class="posfooter">
+    Copyright@<a href="#" target="_blank">2022</a>
+  </div>
+        </div>
+    </div>
+  </footer>
+</body>
+</html>
