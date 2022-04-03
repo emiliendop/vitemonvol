f9afae454944f57931039c284d87608f98b70a91
emiliendop <emiliendop10@gmail.com>
2022-04-04 00:09:02 +0100

    page connexion et inscription

--- View/register.php
+++ View/register.php
@@ -1,89 +1,113 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>HOME</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="../Public/css/w3.css">
-<link rel="stylesheet" href="../Public/css">
-<link rel="stylesheet" href="../Public/css/home.css">
-<style>
+
+<!-- STYLE CSS -->
+
+    <link rel="stylesheet" href="../Public/css/w3.css">
+    <link rel="stylesheet" href="../Public/css">
+    <link rel="stylesheet" href="../Public/css/login.css">
+    <link rel="stylesheet" href="../Public/css/details.css">
+    <style>
 	body, html,h1{height: 100%;
 font-style: italic;
-font-family: "Amatic SC", sans-serif;}
+font-family: "Amatic SC", sans-serif;
+background-color: black;
+
+font-size:25px;}
 </style>
+    <title>LOGIN</title> 
 </head>
 <body>
-  <nav>
+
+<nav>
   
      
-      <div class="w3-top">
-  <div class="w3-bar  w3-black w3-xlarge">
-  <a href="home.php" class="w3-bar-item w3-button"><i>Accueil</i></a>
-		  <a href="#circuit" class="w3-bar-item w3-button"><i>Circuits</i></a>
-		  <a href="#about" class="w3-bar-item w3-button"><i>A Propos</i></a>
-		  <a href="#contact" class="w3-bar-item w3-button"><i>Contact</i></a>
-          <div class="w3-dropdown-hover w3-hide-small">
-    <button class="w3-button" > <i  class="fa fa-user"></i> <i class="fa fa-caret-down"></i></button>     
-    <div class="w3-dropdown-content w3-card-4 w3-bar-block">
-    <a href="login.php" class="w3-bar-item w3-button">Connexion</a>
-      <a href="register.php" class="w3-bar-item w3-button">Inscription</a>
-     
-      <a href="#" class="w3-bar-item w3-button">Reservation</a>
-      
-    </div>
-  </div>
-		  <div style="float: right!important;">
-      <a class="w3-bar-item "><i> AGENCE DE VOYAGE</i></a>
+  <div class="w3-top">
+<div class="w3-bar  w3-black w3-xlarge">
+<a href="home.php" class="w3-bar-item w3-button"><i>Accueil</i></a>
+      <a href="../View/home.php#circuit" class="w3-bar-item w3-button"><i>Circuits</i></a>
+      <a href="#about" class="w3-bar-item w3-button"><i>A Propos</i></a>
+      <a href="#contact" class="w3-bar-item w3-button"><i>Contact</i></a>
+      <div class="w3-dropdown-hover w3-hide-small">
+<button class="w3-button" > <i  class="fa fa-user"></i> <i class="fa fa-caret-down"></i></button>     
+<div class="w3-dropdown-content w3-card-4 w3-bar-block">
+<a href="login.php" class="w3-bar-item w3-button">Connexion</a>
+  <a href="register.php" class="w3-bar-item w3-button">Inscription</a>
+  <a href="#" class="w3-bar-item w3-button">Reservation</a>
+  
+</div>
+</div>
+       <div style="float: right!important;">
+      <a class="w3-bar-item" style="text-decoration:none"><i>AGENCE DE VOYAGE</i></a>
 		  </div>
-  </div>
-		
-  </nav>
-
-
+</div>
+    
+</nav>
 <div class="w3-display-container" style="margin-bottom:50px">
-  <img src="../Public/images/vol.jpeg" style="width:100%" class="dim">
+<img src="../Public/images/voyage.webp" alt="" class="image">
   <div class="w3-display-bottomleft w3-container w3-amber  w3-hide-small"
-   style="bottom:5%;opacity:0.7;width:70%">
-  <h2><b>Voyagez à travers le monde<br>comme vous l'imaginez </b></h2>
+   style="bottom:3%;width:100%">
+   <h2 class="center">INSCRIPTION</h2>
 </div>
 </div>
-<div id="circuit">
-<div class="w3-row w3-container" style="margin:50px 0">
-<div class="w3-third w3-container">
-  <div class="w3-topbar w3-border-amber">
-    <img src="../Public/images/archipel.jpg" style="width:100%" class="img">
-    <h2>Voyage sur l'achipel nippon</h2>
-    <a href="../View/archipel.php"><p>PLUS DE DETAILS</p></a>
+<div class="w3-container w3-black w3-padding-64" >
+    <div class="w3-content">
+    
+      
+      <div class="w3-row">
+       
+          <div class="  w3-padding-large w3-white">
+          <div  style="margin-bottom:70px;margin-top:40px; height: 530px!important">
+    <div  class="w3-half margin-left">
+       <img src="../Public/images/dubai.jpg" style="width:90%;height: 540px!important">
+    </div>
+    <div class="w3-half half">
+    <form>
+  <div class="mb-12">
+    <label >Nom</label>
+    <input type="text" class="form-control" >
   </div>
-</div>
-
-<div class="w3-third w3-container">
-  <div class="w3-topbar w3-border-amber">
-    <img src="../Public/images/grece.jpg" style="width:100%" class="img">
-    <h2>Visite historique en Grèce</h2>
-    <a href="../View/grece.php"><p>PLUS DE DETAILS</p></a>
+  <div class="mb-12">
+    <label class="form-label">Prenom</label>
+    <input type="text" class="form-control" >
   </div>
-</div>
-
-<div class="w3-third w3-container">
-  <div class="w3-topbar w3-border-amber">
-    <img src="../Public/images/caribou.jpg" style="width:100%"  class="img">
-    <h2>Voyage au pays des caribous</h2>
-    <a href="../View/caribou.php"><p>PLUS DE DETAILS</p></a>
+  <div class="mb-12">
+    <label class="form-label">Email</label>
+    <input type="email" class="form-control" >
+    
   </div>
+  <div class="mb-12">
+    <label  class="form-label">Mot De Passe</label>
+    <input type="password" class="form-control">
+  </div>
+  
+  <div class="mb-3 ">
+    Deja un compte? <a href="login.php"><span class="amber">Se connecter</span></a>
+  </div>
+  
+ <a href="#"><button type="submit" class="btn btn-primary">S'incrire</button></a> 
+</form>
+    </div>
 </div>
-</div></div>
+          </div> 
+       
+      </div>
+  
 
 
-
-
-
+  
+    </div>
+   
 </div>
 
-<footer class="w3-container w3-padding-32 w3-grey">  
+<footer class="w3-container w3-padding-32 w3-grey tl">  
     <div class="w3-row-padding">
       
     
@@ -124,10 +148,25 @@ font-family: "Amatic SC", sans-serif;}
     <i class="fa fa-twitter w3-hover-opacity w3-margin-right"></i>
     <i class="fa fa-linkedin w3-hover-opacity w3-margin-right"></i></div>
     <div class="posfooter">
-    Copyright@<a href="#" target="_blank">2022</a>
+    Copyright@<a href="#" target="_blank" style="text-decoration:none">2022</a>
   </div>
         </div>
     </div>
   </footer>
-</body>
+
+  </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
